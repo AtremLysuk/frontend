be56579ac819dbac36976ef721cede887728a178
Artem Lysuk <artemlysuk36@gmail.com>
2026-03-04 19:05:09 +0200

    Fix double animation in PageTransition

--- frontend/src/components/PageTransition/PageTransition.jsx
+++ frontend/src/components/PageTransition/PageTransition.jsx
@@ -3,21 +3,35 @@ import gsap from 'gsap';
 
 const PageTransition = ({children}) => {
   const container = useRef(null);
+
   useEffect(() => {
-    gsap.fromTo(container.current, {
-      opacity: 0,
-      x: -30,
-      filter: 'blur(4px)'
-    }, {
-      opacity: 1,
-      x: 0,
-      filter: 'blur(0px)',
-      duration: 0.35,
-      ease: 'power3.out'
-    });
+
+    const ctx = gsap.context(() => {
+      gsap.fromTo(container.current,
+        {
+          opacity: 0,
+          x: -30,
+          filter: 'blur(4px)'
+        },
+        {
+          opacity: 1,
+          x: 0,
+          filter: 'blur(0px)',
+          duration: 0.35,
+          ease: 'power3.out',
+          delay: 0.1
+        }
+      );
+    }, container);
+
+    return () => ctx.revert();
   }, []);
-  return (<div ref={container}>
-    {children}
-  </div>);
+
+  return (
+    <div ref={container}>
+      {children}
+    </div>
+  );
 };
+
 export default PageTransition;
